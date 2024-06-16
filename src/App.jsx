8d435996ab43802032124c0ad951a60b522bff11
suraj-prkaash-singh <shivamchaudhary8476@gmail.com>
2024-06-17 01:36:35 +0530

    implemented basic text tracing using a div instead of input

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import Home from "./components/Home"
+import HomeV2 from "./components/HomeV2"
+import Test from "./components/Test"
 function App() {
   return (
     <Home/>
+    // <Test/>
   )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,18 @@
+import Test from "./Test";
+
 const Home = () => {
   return (
-    <div className="w-screen h-screen bg-black flex-row font-jetbrains items-center justify-between">
-      <div className="w-full h-max mt-10 ml-8">
+    <div className="font-jetbrains relative">
+      <div className="mt-10 ml-8">
         <p className="text-white text-3xl w-full h-max font-bold">
           <span className="text-[#854091]">Finger</span>Bolt
         </p>
       </div>
-      <div className="text-[#854091] w-full h-max items-center flex justify-center">
+      <div className="text-[#854091] items-center flex justify-center absolute left-[40%] top-[250%]">
         <p className="text-white bg-[#854091] px-8 py-1 rounded-md font-bold cursor-pointer">Type the text below</p>
+      </div>
+      <div className="w-full">
+        <Test/>
       </div>
     </div>
   );
